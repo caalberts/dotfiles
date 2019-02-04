def1ee7bc27b4f6d63bb892acdc9fbe1a3dbdd76
Albert Salim <albertlimca@gmail.com>
2019-02-04 20:13:18 +0700

    remove tsf

--- fish/.config/fish/functions/tsf.fish
+++
@@ -1,4 +0,0 @@
-function tsf -d "Search and attach to a tmux session"
-  tmux list-sessions | sed -E 's/:.*$//' | grep -v '^'(tmux display-message -p "#S")'$' | fzf --reverse | xargs tmux switch-client -t
-end
-
